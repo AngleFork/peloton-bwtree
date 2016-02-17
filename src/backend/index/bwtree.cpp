2822aa06285f5923f11832808e086f650a20723f
ZheyuanBu <buzheyuan@gmail.com>
2016-02-17 19:52:25 +0000

    Insert/delete implementation

--- src/backend/index/bwtree.cpp
+++ src/backend/index/bwtree.cpp
@@ -15,7 +15,51 @@
 namespace peloton {
 namespace index {
 
-// Add your function definitions here
+template <typename KeyType, typename ValueType, class KeyComparator>
+void BWTree<KeyType, ValueType, KeyComparator>::insert_data(const PairType &x) {
+
+  if (m_root == NULL_PID) {
+      m_root = m_headleaf = m_tailleaf = allocate_leaf();
+  }
+
+  KeyType key = x.first;
+  ValueType value = x.second;
+
+  PID curr_pid = m_root;
+  node *curr_node = get_node(m_root);
+
+  while (!curr_node.is_leaf()) {
+    unsigned short slot = find_lower(curr_node, key);
+    curr_pid = curr_node->child_pid[slot];
+    curr_node = get_node(curr_pid);
+  }
+
+  insert_node *delta = allocate_insert(0, x, curr_node);
+  set_node(curr_pid, delta);
+
+}
+
+template <typename KeyType, typename ValueType, class KeyComparator>
+void BWTree<KeyType, ValueType, KeyComparator>::delete_key(const KeyType &x) {
+
+  if (m_root == NULL_PID) {
+      m_root = m_headleaf = m_tailleaf = allocate_leaf();
+  }
+
+  PID curr_pid = m_root;
+  node *curr_node = get_node(m_root);
+
+  while (!curr_node.is_leaf()) {
+    unsigned short slot = find_lower(curr_node, x);
+    curr_pid = curr_node->child_pid[slot];
+    curr_node = get_node(curr_pid);
+  }
+
+  delete_node *delta = allocate_delete(0, x, curr_node);
+  set_node(curr_pid, delta);
+
+}
+
 
 }  // End index namespace
 }  // End peloton namespace
--- src/backend/index/bwtree.h
+++ src/backend/index/bwtree.h
@@ -19,6 +19,7 @@
 #include <memory>
 #include <cstddef>
 #include <assert.h>
+#include <unordered_map>
 
 #define BWTREE_MAX(a,b) ((a) < (b) ? (b) : (a))
 #define BWTREE_NODE_SIZE 256
@@ -26,8 +27,6 @@
 namespace peloton {
 namespace index {
 
-// Look up the stx btree interface for background.
-// peloton/third_party/stx/btree.h
 template <typename KeyType, typename ValueType, class KeyComparator>
 class BWTree {
 
@@ -42,6 +41,8 @@ public:
 
 public:
   // *** Static Constant Options and Values of the BW Tree
+
+  static const PID NULL_PID = 0;
 
   static const unsigned short leaf_slot_max = BWTREE_MAX(8, BWTREE_NODE_SIZE / (sizeof(KeyType) + sizeof(ValueType)));
 
@@ -80,6 +81,8 @@ private:
   /// data items.
   struct inner_node : public node {
 
+    typedef typename AllocType::template rebind<inner_node>::other alloc_type;
+
     KeyType slot_key[inner_slot_max];
 
     PID child_pid[inner_slot_max + 1];
@@ -105,9 +108,11 @@ private:
   /// data items.
   struct leaf_node : public node {
 
-    leaf_node *prev_leaf;
+    typedef typename AllocType::template rebind<leaf_node>::other alloc_type;
 
-    leaf_node *next_leaf;
+    PID prev_leaf;
+
+    PID next_leaf;
 
     KeyType slot_key[leaf_slot_max];
 
@@ -115,7 +120,7 @@ private:
 
     inline void initialize() {
       node::initialize(0, false);
-      prev_leaf = next_leaf = NULL;
+      prev_leaf = next_leaf = NULL_PID;
     }
 
     inline bool isfull() const {
@@ -149,11 +154,22 @@ private:
       node::initialize(l, true);
       base = NULL;
     }
+
+    inline void initialize(const unsigned short l, const node *n) {
+      node::initialize(l, true);
+      base = n;
+    }
+
+    inline void set_base(const node *n) {
+      base = n;
+    }
   };
 
   /// Extended structure of a delta node in memory. Contains a key, value
   /// pair to insert
   struct insert_node : public delta_node {
+    typedef typename AllocType::template rebind<insert_node>::other alloc_type;
+
     KeyType insert_key;
     ValueType insert_value;
 
@@ -166,6 +182,8 @@ private:
 
   /// Extended structure of a delta node in memory. Contains a key to delete
   struct delete_node : public delta_node {
+    typedef typename AllocType::template rebind<delete_node>::other alloc_type;
+
     KeyType delete_key;
 
     inline void initialize(const unsigned short l, const KeyType &key) {
@@ -201,6 +219,153 @@ private:
       leaf = pid;
     }
   };
+
+private:
+  // *** Tree Object Data Members
+
+  /// Logical pointer to the BW tree's root node, either leaf or inner node
+  PID m_root;
+
+  /// Logical pointer to first leaf in the double linked leaf chain
+  PID m_headleaf;
+
+  /// Logical pointer to last leaf in the double linked leaf chain
+  PID m_tailleaf;
+
+  /// Memory allocator.
+  AllocType m_allocator;
+
+  /// Mapping table
+  std::unordered_map<PID, node *> mapping_table;
+
+public:
+
+  /// Default constructor initializing an empty BW tree with the standard key
+  /// comparison function
+  explicit inline BWTree(const AllocType &alloc = AllocType())
+      : m_root(NULL_PID), m_headleaf(NULL_PID), m_tailleaf(NULL_PID), m_allocator(alloc) {
+  }
+
+
+  /// Frees up all used B+ tree memory pages
+  inline ~BWTree() {
+      // clear();
+  }
+
+private:
+  // *** Convenient Key Comparison Functions Generated From key_less
+
+  /// True if a < b ? "constructed" from m_key_less()
+  inline bool key_less(const KeyType &a, const KeyType b) const {
+    return KeyComparator(a, b);
+  }
+
+  /// True if a <= b ? constructed from key_less()
+  inline bool key_lessequal(const KeyType &a, const KeyType b) const {
+    return !KeyComparator(b, a);
+  }
+
+  /// True if a > b ? constructed from key_less()
+  inline bool key_greater(const KeyType &a, const KeyType &b) const {
+    return KeyComparator(b, a);
+  }
+
+  /// True if a >= b ? constructed from key_less()
+  inline bool key_greaterequal(const KeyType &a, const KeyType b) const {
+    return !KeyComparator(a, b);
+  }
+
+  /// True if a == b ? constructed from key_less(). This requires the <
+  /// relation to be a total order, otherwise the B+ tree cannot be sorted.
+  inline bool key_equal(const KeyType &a, const KeyType &b) const {
+    return !KeyComparator(a, b) && !KeyComparator(b, a);
+  }
+
+
+private:
+  // *** Node Object Allocation and Deallocation Functions
+
+  typename leaf_node::alloc_type leaf_node_allocator() {
+    return typename leaf_node::alloc_type(m_allocator);
+  }
+
+  /// Return an allocator for inner_node objects
+  typename inner_node::alloc_type inner_node_allocator() {
+    return typename inner_node::alloc_type(m_allocator);
+  }
+
+  /// Return an allocator for inner_node objects
+  typename insert_node::alloc_type insert_node_allocator() {
+    return typename insert_node::alloc_type(m_allocator);
+  }
+
+  /// Return an allocator for inner_node objects
+  typename delete_node::alloc_type delete_node_allocator() {
+    return typename delete_node::alloc_type(m_allocator);
+  }
+
+  /// Allocate and initialize a leaf node
+  inline PID allocate_leaf() {
+    leaf_node *n = new (leaf_node_allocator().allocate(1)) leaf_node();
+    n->initialize();
+    PID pid = allocate_pid();
+    mapping_table.insert(std::make_pair(pid, n));
+    return pid;
+  }
+
+  /// Allocate and initialize an inner node
+  inline PID allocate_inner(unsigned short level) {
+    inner_node *n = new (inner_node_allocator().allocate(1)) inner_node();
+    n->initialize(level);
+    PID pid = allocate_pid();
+    mapping_table.insert(std::make_pair(pid, n));
+    return pid;
+  }
+
+  /// Allocate and initialize an insert delta node
+  inline insert_node *allocate_insert(unsigned short level, node *base) {
+    insert_node *n = new (insert_node_allocator().allocate(1)) insert_node();
+    n->initialize(level, base);
+    return n;
+  }
+
+  /// Allocate and initialize an insert delta node
+  inline delete_node *allocate_delete(unsigned short level, node *base) {
+    insert_node *n = new (insert_node_allocator().allocate(1)) delete_node();
+    n->initialize(level, base);
+    return n;
+  }
+
+private:
+
+  inline PID allocate_pid() {
+    PID pid = NULL_PID;
+    while (mapping_table.find(++pid) == mapping_table.end())
+      ;
+    return pid;
+  }
+
+  inline node *get_node(PID pid) {
+    if (mapping_table.find(pid) == mapping_table.end())
+      return NULL;
+    return mapping_table[pid];
+  }
+
+  inline void set_node(PID pid, node *n) {
+    mapping_table[pid] = n;
+  }
+
+private:
+  template <typename node_type>
+  inline unsigned short find_lower(const node_type *n, const KeyType &key) const{
+    unsigned short lo = 0;
+    while (lo < n->slotuse && key_less(n->slotkey[lo], key)) ++lo;
+    return lo;
+  }
+
+public:
+  inline void insert_data(const PairType &x);
+  inline void delete_key(const KeyType &x);
 
 };
 
